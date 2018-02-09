999b225f988f755f4230a9e80bd375778edfd2a5
EthanShipston <36234265+EthanShipston@users.noreply.github.com>
2018-02-09 09:03:56 -0500

    Added .xaml and .cs code

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿/*
+Ethan Shipston
+2/8/2018
+u1_02_ShipstonOutput
+A program that says hello to you when you type your name into it's entry slot.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace u1_02_ShipstonOutput
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnShowGreeting_Click(object sender, RoutedEventArgs e)
+        {
+            lblOutput.Content = "Hello, " + entryName.Text + "!";
+        }
+    }
+}
